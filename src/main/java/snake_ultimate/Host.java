feef85b2bfab2142653725fd02bdae972a2a68e4
mondk <66952082+mondk@users.noreply.github.com>
2023-01-13 10:57:12 +0100

    stuff

---
+++ src/main/java/snake_ultimate/Host.java
@@ -0,0 +1,180 @@
+package snake_ultimate;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.*;
+
+import org.jspace.ActualField;
+import org.jspace.FormalField;
+import org.jspace.SequentialSpace;
+import org.jspace.SpaceRepository;
+
+public class Host implements Runnable{
+
+	private ArrayList<PlayerInfo> players;
+	private String uri;
+	byte map[][];
+	Host(String uri){
+		this.uri=uri;
+		this.players=new ArrayList<>();
+	}
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+try {
+			
+		
+			// Create a repository 
+			SpaceRepository repository = new SpaceRepository();
+			 
+			SequentialSpace queue = new SequentialSpace();
+
+			repository.add("queue", queue);
+		
+			URI myUri = new URI(uri);
+			String gateUri = "tcp://" + myUri.getHost() + ":" + myUri.getPort() +  "?keep" ;
+			System.out.println("Opening repository gate at " + gateUri + "...");
+			repository.addGate(gateUri);
+
+			// queuing players 
+			while (queue.queryp(new ActualField("start"))==null) {
+			
+				
+				Object[] p = queue.getp(new ActualField("join"),new FormalField(String.class),new FormalField(Integer.class));
+				if(p!=null) {
+					players.add(new PlayerInfo((String)p[1]));
+					System.out.println("Joined");
+				}
+			}
+			
+			//creating tupleSpaces
+			for(PlayerInfo p:players) {
+				
+				repository.add(p.name+"_positions", p.posistion);
+				repository.add(p.name+"_movement", p.movement);
+				
+			}
+			queue.put("begin");
+
+			//players.get(0).movement.put("a");
+			//String we ="e";
+			//while(true) {
+				//System.out.println(players.get(0).name+": "+players.get(0).movement.query(new FormalField(String.class))[0]);
+				//if(we.equals("er")){
+					//break;
+				//}
+		//	}
+			
+
+			//GameLoop
+			while(true) {
+				//update player position
+				Thread.sleep(300);
+				for(PlayerInfo p:players) {
+					String input;
+					Object[] t = p.movement.query(new FormalField(String.class));
+					if(t!=null) {
+						input=(String) t[0];
+						if(input.equals("a")) {
+							p.increaseAngle();
+						}
+						else if(input.equals("d")){
+							p.decreaseAngle();
+						}
+					}
+					//move player
+					p.move();
+				
+					
+					//update map
+				
+				
+				}
+				
+				
+				
+				
+				//collision is checked for all players
+				//checkCollision(players);
+				
+				for(PlayerInfo p: players) {
+					for(PlayerInfo q: players) {
+						p.posistion.put(q.x,q.y);
+					}
+				}
+				Thread.sleep(1000);
+				
+			}
+
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		} catch (URISyntaxException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		
+	}
+	
+	//check player collision
+	public void checkCollision(ArrayList<PlayerInfo> players) {
+		for(PlayerInfo p: players) {
+			for(int m = -p.thickness/2; m <= p.thickness/2; m++) {
+				for(int n = -p.thickness; n <= p.thickness/2; n++) {
+					if(Math.ceil(Math.sqrt(m*m + n*n)) == p.thickness) {
+						if(map[p.x + m][p.y + n] != 0 && map[p.x +m][p.y + n] != p.playernumber) {
+							//dead;
+						}
+						//cannot colide with itself
+					}
+				}
+			}
+		}
+	}
+	
+	
+	public class PlayerInfo{
+		double angle;
+		byte playernumber;
+		String name;
+		SequentialSpace posistion;
+		SequentialSpace movement;
+		double forceX;
+		double forceY;
+		double force = 5;
+		int thickness;
+		int x;
+		int y;
+		
+		PlayerInfo(String name){
+			this.name=name;
+			this.angle=0;
+			this.posistion = new SequentialSpace();
+			this.movement = new SequentialSpace();
+			this.forceX = 1;
+			this.forceY=1;
+		}
+		public void decreaseAngle() {
+			// TODO Auto-generated method stub
+			angle-=12;
+		}
+		public void increaseAngle() {
+			// TODO Auto-generated method stub
+			angle+=12;
+		}
+		public void move() {
+			forceX=Math.cos(Math.toRadians(angle));
+			forceY=Math.sin(Math.toRadians(angle));
+			y+=forceY*force;
+			x+=forceX*force;
+			
+		}
+		
+	}
+
+	
+
+}
---
+++ src/main/java/snake_ultimate/PlayerInGame.java
@@ -0,0 +1,219 @@
+package snake_ultimate;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.UnknownHostException;
+import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+import javax.swing.*;
+import javax.imageio.IIOException;
+import org.jspace.*;
+
+public class PlayerInGame implements Runnable{
+	
+	private static String name;
+	private  RemoteSpace position;
+	private RemoteSpace movement;
+	private int numOfPlayers;
+	
+	PlayerInGame(int numberOfPlayers,String name,RemoteSpace position,RemoteSpace movement){
+		this.name=name;
+		this.position=position;
+		this.movement=movement;
+		this.numOfPlayers=numberOfPlayers;
+		
+	}
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+		new Thread(new DrawThread(numOfPlayers, position,movement)).start();
+		
+	}
+
+
+	
+
+	
+	public class Control implements KeyListener{
+
+		RemoteSpace movement;
+		
+		Control(RemoteSpace movement){
+			this.movement=movement;
+		}
+		@Override
+		public void keyTyped(KeyEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void keyPressed(KeyEvent e) {
+			// TODO Auto-generated method stub
+			if(e.getKeyCode()==KeyEvent.VK_LEFT) {
+				input("a");
+			}
+			else if(e.getKeyCode()==KeyEvent.VK_RIGHT) {
+				input("d");
+			}
+			
+		}
+
+		@Override
+		public void keyReleased(KeyEvent e) {
+			// TODO Auto-generated method stub
+			if(e.getKeyCode()==KeyEvent.VK_LEFT) {
+				input(" ");
+			}
+			
+			else if(e.getKeyCode()==KeyEvent.VK_RIGHT) {
+				input(" ");
+			
+		}
+			
+		
+		
+	}
+		public void input(String i) {
+			try {
+				this.movement.getp(new FormalField(String.class));
+				this.movement.put(i);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+	}
+	}
+
+	/*class DrawThread implements Runnable {
+		int formerPosX[];
+		int formerPosY[];
+		int numPlayers = 2;
+
+		private RemoteSpace position;
+
+		
+	    public DrawThread(int numPlayers,RemoteSpace position) {
+	    	this.numPlayers = numPlayers;
+	    	this.position=position;
+	    	
+	    	this.formerPosX = new int[numPlayers];
+	    	this.formerPosY = new int[numPlayers];
+	    	for(int i = 0; i < numPlayers; i++) {
+	  //  		formerPosX[i] = startPosX[i];
+	  //   		formerPosY[i] = startPosY[i];
+	    	}
+	    	
+	  //draw Start Pos
+	    	
+	    }
+
+	    public void run() {
+	    	int playerPosx[] = new int[numPlayers];
+	    	int playerPosy[] = new int[numPlayers];
+			
+	    	while(true) {
+
+	    		try {
+					for(int i = 0; i < numPlayers; i++){
+						
+						Object[] t = position.get(new FormalField(Integer.class),new FormalField(Integer.class));
+						
+						playerPosx[i]=(int) t[0];
+						playerPosy[i]=(int) t[1];
+							
+					}
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+
+	    		for(int i = 0; i < numPlayers; i++){
+
+	    		}
+	    		
+	    		
+	    	}
+
+	    		//draw lines from formerPos to newPos for all players
+	    		//delete and update circles to newPos
+	    		//formerPos = newPos
+	    }
+
+	}
+	*/
+	class DrawThread extends JPanel implements Runnable{
+
+		int formerPosX[];
+		int formerPosY[];
+		int numPlayers = 1;
+		int x [] = {100, 266, 633, 900}; //start positions
+		int y [] = {100, 400, 400, 100};
+		Color color[] = {Color.RED, Color.BLUE, Color.GREEN, Color.YELLOW};
+		private RemoteSpace position;
+		private RemoteSpace movement;
+		
+	    public DrawThread(int numPlayers,RemoteSpace position,RemoteSpace movement) {
+	    	this.numPlayers = numPlayers;
+	    	this.position=position;
+	    	this.movement=movement;
+	    	
+	    	
+			JFrame j = new JFrame();
+			j.setBackground(Color.WHITE);
+			j.setSize(1000,1000);
+			j.add(this);
+			j.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			j.setResizable(false);
+			j.setVisible(true);
+			j.addKeyListener(new Control(movement));
+		}
+		
+
+				
+		protected void paintComponent(Graphics g){
+			for(int i = 0; i < 4; i++) {
+				g.setColor(color[i]);
+				drawCircle(g,x[i],y[i],5);
+			}
+			
+		}
+		
+		 public void drawCircle(Graphics cg, int xCenter, int yCenter, int r) {
+		     cg.drawOval(xCenter-r, yCenter-r, 2*r, 2*r);
+		     cg.fillOval(xCenter-r, yCenter-r, 2*r, 2*r);
+		 }//end drawCircle
+		
+		public void run(){
+			try {
+				while(true){
+					Thread.sleep(300);
+					repaint();
+					for(int i = 0; i < numPlayers; i++) {
+						Object[] t;
+
+						t = position.get(new FormalField(Integer.class),new FormalField(Integer.class));
+						x[i] = (int) t[0];
+						y[i] = (int) t[1];
+						
+					}
+				}
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+		}
+
+	
+	}
+}
+	
+
+
+
+
